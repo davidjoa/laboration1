1d7276cb2fac74e2aff25a85213ac85dbcb7323b
davidjoa <joakim_davidsson@hotmail.com>
2015-09-25 13:06:31 +0000

    uppgift 1 moment 1och2

---
+++ scripts/lab.js
@@ -0,0 +1,2 @@
+console.log("Hello World");
+alert("Rad 1\nRad 2");
--- scripts/min.js
+++ scripts/min.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
  $(".right").mouseenter(function() {
   alert("Badass tree");
  });
-});
+});*/
 
 
 
-$(document).ready(function() {
+/*$(document).ready(function() {
  $("#meny").click(function() {
 
   $("#navigation").slideToggle("slow");
@@ -15,10 +15,59 @@ $(document).ready(function() {
 
 $(document).ready(function() {
  $("button").click(function() {
-  $("#div1").fadeToggle("slow");
-  $("#div2").fadeToggle(3000);
-  $("#div3").fadeToggle(3000);
+  $(".right").fadeToggle("slow");
+  $(".left").fadeToggle(3000);
+  $("#fadesmile").fadeToggle(2000);
+  $("#div1").fadeToggle(1000);
+
+  $("#div3").fadeToggle(1000);
 
 
  });
 });
+
+$(document).ready(function() {
+ $("button").click(function() {
+  $("#div2").animate({
+   left: '150px'
+
+  });
+ });
+});
+
+$(document).ready(function() {
+ $("#div2").click(function() {
+  $(this).hide();
+ });
+})
+
+var dark = $("form :input");
+
+$(document).ready(function() {
+ $(dark).focus(function() {
+  $(this).css("background-color", "#cccccc");
+ });
+ $(dark).blur(function() {
+  $(this).css("background-color", "#ffffff");
+ });
+});
+
+$(document).ready(function() {
+ console.log("dokumentet färdigladdat");
+});
+
+/*var bull = $("ul li").not("a");
+
+$(document).ready(function() {
+ $(bull).click(function() {
+  $(this).hide();
+ });
+
+});*/
+
+/*$("#content")
+ .find("h4")
+ .eq(1)
+ .html("ny text,test");*/
+ 
+ 
