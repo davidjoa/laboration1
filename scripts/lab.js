71910cee00f913d13df205c13f72ad7263f1bd8b
davidjoa <joakim_davidsson@hotmail.com>
2015-09-28 12:18:23 +0000

    on my way marodören task 1to4

--- scripts/lab.js
+++ scripts/lab.js
@@ -1,2 +1,78 @@
-console.log("Hello World");
-alert("Rad 1\nRad 2");
+/*console.log("Hello World"); //Uppgift 1 moment 1
+alert("Rad 1\nRad 2"); //Uppgift 1 moment 2*/
+
+
+/*var average = function (a,b,c,d){
+
+var val = (a+b+c+d) / 4;
+console.log(val);
+};
+
+average(4,4,4,4); */ //Uppgift 2 moment 1. Är denna OK?
+
+/*var average = function(a,b,c,d) {
+    return (a+b+c+d) / 4;
+};
+
+var answer = average(4,4,4,4);
+console.log(answer);*/ //Uppgift 2 moment 1, Eller denna?
+
+
+
+
+
+/*var fras = function(text1, text2) {
+    console.log(text1 + text2);
+};
+
+fras("Funkar", ",fint")*/ //Uppgift 2 moment 2
+
+/*var number =Math.floor(Math.random() * 100)
+var correctAnswer=false
+while(!correctAnswer)  
+{
+    var guess = prompt("Guess what number i'm thinking off between 1 - 100");
+   
+    if (guess <number){
+        alert("Higher!");
+            }
+        
+    else if (guess >number){
+            alert("Lower!");
+            }
+            
+    else{
+                alert("Correct guess, lucky you!")
+                correctAnswer=true;
+                }}*/
+
+/*prompt("0.Exit\n\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Guess number"); // kör in en loop här också..
+var userChoice = true
+{
+    
+    if (userChoice === "0") {
+    alert("Bye bye");
+    userChoice=false
+}
+else {
+    prompt("0.Exit\n\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Guess number");
+
+}
+}
+*/
+/*do {
+    userChoice = prompt(menuText);
+    switch(userChoice) {
+        case "1":
+            fahrenheitToCelsius();
+            break;
+        case "2":
+            celsiusToFahrenheit();
+            break;
+        case "3":
+            playGuessNumber();
+            break;
+        default:
+    }
+} while(userChoice !== "0")*/
+
