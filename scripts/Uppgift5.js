40b193a011b0cab487f311813f247dc496eff606
davidjoa <joakim_davidsson@hotmail.com>
2015-09-29 08:12:55 +0000

    on my way uppgift5

---
+++ scripts/Uppgift5.js
@@ -0,0 +1,15 @@
+var newString = "Jag tEsTaR lITe hÄr";
+var UpperToLowerLowerToUpper = function(){
+    
+    
+    }
+
+
+var res = newString.replace(/[aA]/g, "#");
+
+console.log(res)
+
+
+//.replace(/[aA]/g, "#");
+
+//+operator gör om siffra till sträng
