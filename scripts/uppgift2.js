a34a9de3387d82d5f3939301fab0d00e78a20ebb
davidjoa <joakim_davidsson@hotmail.com>
2015-10-01 06:50:42 +0000

    done task 9, done lab Marodören

--- scripts/uppgift2.js
+++ scripts/uppgift2.js
@@ -4,11 +4,11 @@ var val = (a+b+c+d) / 4;
 return(val);
 };
 
-console.log(average(4,4,4,4));
+console.log(average(7,4,10,13));
 
 
 var fras = function(text1, text2) {
     console.log(text1 + text2);
 };
 
-fras("Funkar", ",fint")
+fras("Funkar", ",bra")
--- scripts/uppgift9.js
+++ scripts/uppgift9.js
@@ -1,24 +1,38 @@
-
-
 var tInput = [
     ["Förnamn", "Efternamn", "Telefon"],
     ["Joakim", "Davidsson", 55555],
     ["Haris", "Kljajic", 33333],
     ["Robert", "Lewa", 99999],
     ["Ida", "Svensson", 77777],
-    ["Mikael", "Svensson", 98765]
+    ["Mikael", "Svensson", 98765],
+    ["Pennan", "Pennsson", 56789],
+    ["Testperson", "Testsson", 98765]
 ];
 
 
+generateTable(tInput);
 
-var generateTable = function(tInput) {
+function generateTable(tBook) {
 
-    console.log(tInput);
+    document.write("<table><thead><tr>");
 
-    var table = document.write("<table><thead><tr></tr>");
-
-    for (var i = 0; i < tInput.length; i++) {
-        (tInput[i] += table);
-
+    for (var i = 0; i < 3; i++) {
+        document.write("<th>" + tBook[0][i] + "</th>");
     }
-};
+    document.write("</tr></thead>");
+    document.write("<tbody>");
+
+
+    for (var i = 1; i < tInput.length; i++) {
+        if (i % 2 == 0) {
+            document.write("<tr bgcolor= \"#BBB\">");
+        }
+
+        for (var j = 0; j < 3; j++) {
+            document.write("<td>" + tBook[i][j] + "</td>");
+        }
+        document.write("</tr>");
+    }
+    document.write("</tbody></table>");
+
+}
